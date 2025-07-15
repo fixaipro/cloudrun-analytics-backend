e521d7ae8e713afeddef425e4e294d4e7e944a34
fixaipro <fixaipro6@gmail.com>
2025-07-16 00:38:59 +0100

    Delete analytics_modules/all_analysis_scripts.py

--- analytics_modules/all_analysis_scripts.py
+++
@@ -1,7 +0,0 @@
-def analysis_a(file_url):
-    # TODO: fetch file_url, process, and generate HTML for type A
-    return f"<html><body><h1>Analysis A</h1><p>Processed {file_url}</p></body></html>"
-
-def analysis_b(file_url):
-    # TODO: fetch file_url, process, and generate HTML for type B
-    return f"<html><body><h1>Analysis B</h1><p>Processed {file_url}</p></body></html>"
